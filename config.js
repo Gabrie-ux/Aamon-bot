02a5b4ea30bbd6b872269cb305333493acac7b2d
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-04-13 10:55:27 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import { tr, translateText } from './lib/_checkLang.js';
 
 //---------[ Añada los numeros a ser Propietario/a ]---------
 
-global.owner = [['51941658192', 'ＰＲＯＰＩＥＴＡＲＩＯ', true], ['51941658192','JonathanG', true], ['5216633900512','BrayanOFC',true], [''], [''], [''], [''], [''], [''], [''], [''], ['']]
+global.owner = [['50557333744', 'ＰＲＯＰＩＥＴＡＲＩＯ', true], ['51941658192','gabriel', true], ['5216633900512','BrayanOFC',true], [''], [''], [''], [''], [''], [''], [''], [''], ['']]
 global.mods = []
 global.prems = []
 
